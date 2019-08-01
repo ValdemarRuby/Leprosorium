436ed8105a6f35c08d152b7d0f1ff7846afa5753
ValdemarRuby <wings6928@gmail.com>
2019-08-01 11:42:23 +0300

    Add validation

--- app.rb
+++ app.rb
@@ -12,6 +12,21 @@ before do
 		init_db
 end
 
+	# configure вызывается каждый раз при конфигурации приложения:
+	# когда изменился код программы и перезагрузилась страница
+
+configure do
+		# иницаилизация бд
+		init_db
+		# создает таблицу если она не существует
+		@db.execute 'create table if not exists Posts
+			(
+				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+				"created_date" DATE,
+				"Content" TEXT
+				)'
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"
 end
@@ -22,6 +37,10 @@ end
 
 post '/new' do
 	content = params[:content]
+	if content.length <= 0
+		@error = "Type text"
+		return erb :new
+	end
 
 	erb "Your typed: #{content}"
 end
