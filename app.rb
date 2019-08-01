0c2a8dbadb1ada27913a208c2feb601ed7f27799
ValdemarRuby <wings6928@gmail.com>
2019-08-01 17:14:25 +0300

    Add display comment

--- app.rb
+++ app.rb
@@ -18,13 +18,22 @@ end
 configure do
 		# иницаилизация бд
 		init_db
-		# создает таблицу если она не существует
+		# создает таблицу для постов если она не существует
 		@db.execute 'create table if not exists Posts
 			(
 				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 				"created_date" DATE,
 				"Content" TEXT
 				)'
+
+		# создаем таблицу для коментариев для постов
+		@db.execute 'create table if not exists Comments
+					(
+						"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+						"created_date" DATE,
+						"Content" TEXT,
+						"post_id" INTEGER
+						)'
 end
 
 get '/' do
@@ -57,8 +66,32 @@ end
 
 # вывод информации по коментарию
 get '/details/:post_id' do
+	# получаем переменную из URL-a
 	post_id = params[:post_id]
+
+	# получаем список постов
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
+
+	# выбираем этот один пост в переменную @row
 	@row = results[0]
+
+	# выбираем коментарии для нашего поста
+	@comments = @db.execute 'select * from Comments where post_id = ?', [post_id]
+
+	# возвращаем представление details.erb
 	erb :details
+end
+
+# обработчик post-запроса /details/...
+# (браузер отправляет данные на сервер, а мы ипринимаем)
+post '/details/:post_id' do
+	post_id = params[:post_id]
+	content = params[:content]
+	# if content.length <= 0
+	# 	@error = "Type post text"
+	#
+	# 	return erb :details
+	# end
+	@db.execute 'insert into Comments (content, created_date, post_id) values (?, datetime(), ?)', [content, post_id]
+	erb "You type comment: #{content}"
 end
